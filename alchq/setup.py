993a0bea7716de4221e35b0a790e318f4066c742
Alex Weavers <alex@pigeon.ly>
2020-11-27 19:41:49 -0800

    Update setuptools

--- alchq/setup.py
+++ alchq/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="alchq-awildtechno",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Alex Weavers",
+    author_email="ajdweavers@gmail.com",
+    description="A distributed queue using SQLAlchemy and a database.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/awildtechno/alchq",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
